6742241f9c9e5f57da356249454ad27430ac9d15
Francis Pastor <fcp3@njit.edu>
2017-09-07 02:11:27 -0400

    Pulling correct data, needs to store in SQL and output

--- webscrape/src/main/java/com/fcp3/app/App.java
+++ webscrape/src/main/java/com/fcp3/app/App.java
@@ -1,10 +1,15 @@
 package com.fcp3.app;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jsoup.Jsoup;
@@ -22,6 +27,21 @@ import org.json.*;
 public class App 
 {
 	
+	String URL = "http://mobile.njit.edu/parking/data.php";
+	Document doc;
+	private static JSONObject json;
+	
+	private static JSONObject getJSONObject(JSONObject json, String obj) {
+		JSONObject jsonObj = null;
+		try {
+			jsonObj = json.getJSONObject("decks").getJSONObject(obj);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return jsonObj;
+	}
+	
     public static void main( String[] args ) throws Exception
     {
         
@@ -31,22 +51,27 @@ public class App
         	System.out.println(doc.select("body").text());
         	String text = doc.select("body").text();
         	
-        	for(Element e: doc.select("div.list-group-item")) {
-        		System.out.println(e.html());
-        		System.out.println(e.select("div.row div").html());
-        	}
-        	JSONObject json = new JSONObject(text);
-        	System.out.println(json.names().toString());
-        	JSONObject sciTechGarage = json.getJSONObject("decks").getJSONObject("0");
-        	JSONObject parkingDeck = json.getJSONObject("decks").getJSONObject("1");
-        	System.out.println(sciTechGarage.toString());
-        	System.out.println(parkingDeck.toString());
+        	json = new JSONObject(text);
+        	System.out.println(json.toString());
+        	
+        	JSONObject sciTechGarage = getJSONObject(json, "0");
+        	JSONObject parkingDeck = getJSONObject(json, "1");
         	
         	//Science and Tech Garage
         	String name1 = sciTechGarage.getString("Name");
         	String available1 = sciTechGarage.getString("Available");
-        	System.out.println(name1 + " : " + available1);
         	
+        	//Parking Deck
+        	String name2 = parkingDeck.getString("Name");
+        	String available2 = parkingDeck.getString("Available");
+        	
+        	
+        	String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
+        	
+        	String out1 = timeStamp + " - " + name1 + " : " + available1;
+        	String out2 = timeStamp + " - " + name2 + " : " + available2;
+        	
+        	//Files.write("output.txt", (out1 + System.lineSeparator()).getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
         	
         		
         } catch (IOException e) {
